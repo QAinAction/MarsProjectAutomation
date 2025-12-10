6ce8cd6248a1f4765e9545bca80a0e8e8fe4650b
Lija John <lija2512@gmail.com>
2025-12-10 17:22:13 +1100

    Skill tests added

--- Pages/SkillsPage.cs
+++ Pages/SkillsPage.cs
@@ -17,7 +17,7 @@ namespace MarsProject.Pages
         private readonly Helpers helpers;
         public IWebDriver Driver => _driver;
 
-        
+
         // Initializes a new instance of the SkillsPage class with a WebDriver instance.
         public SkillsPage(IWebDriver driver) // Inject IWebDriver directly
         {
@@ -25,6 +25,108 @@ namespace MarsProject.Pages
             wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10)); // 10-second timeout
             helpers = new Helpers(_driver);
         }
-        
+        // Locates and returns the Skill tab element.
+        public IWebElement Skilltabclick(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//div[@class='ui top attached tabular menu']/a[2]"));
+
+        }
+        // Locates and returns the "Add New" button element on the Skills page.
+        public IWebElement SkillAddNewbutton(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/thead/tr/th[3]/div"));
+        }
+        // Locates and returns the skill input field element.
+        public IWebElement Skillfieldvalue(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//input[@placeholder='Add Skill']"));
+        }
+        // Locates and returns the skill level dropdown element as a SelectElement.
+        public SelectElement SkillLevelvalue(IWebDriver driver)
+        {
+            IWebElement SkillLevelvalue = driver.FindElement(By.XPath("//select[@class='ui fluid dropdown']"));
+            SelectElement SkillLevelvalueSelect = new SelectElement(SkillLevelvalue);
+            return SkillLevelvalueSelect;
+        }
+        // Locates and returns the "Add" button element.
+        public IWebElement SkillAddbutton(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//input[@value='Add']"));
+        }
+        // Creates a new skill record by filling in the skill and level, then clicking "Add".
+        public void SkillsPageRecordCreation(string skill, string skilllevel)
+        {
+
+            Skilltabclick(_driver).Click();
+            SkillAddNewbutton(_driver).Click();
+            Skillfieldvalue(_driver).SendKeys(skill);
+            SkillLevelvalue(_driver).SelectByText(skilllevel);
+            SkillAddbutton(_driver).Click();
+        }
+        // Locates and returns the skill value in the last row of the skills table.
+        public IWebElement SkillLastData(IWebDriver driver)
+        {
+            helpers.WaitForElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+
+            return driver.FindElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+
+        }
+        // Locates and returns the edit icon element for the last skill record.
+        public IWebElement SkillEditicon(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[1]"));
+        }
+        // Locates and returns the skill level dropdown element for updating as a SelectElement.
+        public SelectElement SkillLevelUpdate(IWebDriver driver)
+        {
+            IWebElement SkillLevelUpdate = driver.FindElement(By.XPath("//select[@class='ui fluid dropdown']"));
+            SelectElement SkillLevelUpdateSelect = new SelectElement(SkillLevelUpdate);
+            return SkillLevelUpdateSelect;
+
+        }
+        // Locates and returns the "Update" button element.
+        public IWebElement SkillUpdatebutton(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//input[@value='Update']"));
+        }
+        // Updates an existing skill record by changing the skill level and clicking "Update".
+        public void SkillsPageRecordUpdate(string updatedskilllevel)
+        {
+            Skilltabclick(_driver).Click();
+            SkillEditicon(_driver).Click();
+            SkillLevelUpdate(_driver).SelectByText(updatedskilllevel);
+            SkillUpdatebutton(_driver).Click();
+        }
+        // Locates and returns the updated skill level value in the last row of the skills table.
+        public IWebElement SkillDataUpdated(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
+        }
+        // Validates adding a skill without data.
+        public void SkillsPageValidationCheckwithnodata()
+        {
+
+            SkillAddNewbutton(_driver).Click();
+            SkillAddbutton(_driver).Click();
+        }
+        // Validates adding a skill with provided data.
+        public void SkillsPageValidationCheckwithdata(string skill, string skilllevel)
+        {
+            Skillfieldvalue(_driver).SendKeys(skill);
+            SkillLevelvalue(_driver).SelectByText(skilllevel);
+            SkillAddbutton(_driver).Click();
+
+
+        }
+        // Locates and returns the delete icon element for the last skill record.
+        public IWebElement SkillDeleteIcon(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[2]"));
+        }
+        // Deletes the last skill record by clicking the delete icon.
+        public void SkillsPageDeleteRecord()
+        {
+            SkillDeleteIcon(_driver).Click();
+        }
     }
 }
--- StepDefinitions/SkillsStepDefinitions.cs
+++ StepDefinitions/SkillsStepDefinitions.cs
@@ -18,13 +18,103 @@ namespace MarsProject.StepDefinitions
 
         public SkillsStepDefinitions(IWebDriver driver)
         {
-        this.driver= driver;
-        this.baseClass= new BaseClass(driver);
-            loginPage= new LoginPage(driver);
-            skillPage= new SkillsPage(driver);
-            helpers= new Helpers(driver);
+            this.driver = driver;
+            this.baseClass = new BaseClass(driver);
+            loginPage = new LoginPage(driver);
+            skillPage = new SkillsPage(driver);
+            helpers = new Helpers(driver);
         }
-       
+        [Then("the user should be redirected to the Profile page with Profile name {string}")]
+        public void ThenTheUserShouldBeRedirectedToTheProfilePageWithProfileName(string ExpectedProfilename)
+        {
+            helpers.WaitForElement(By.XPath("//div[@id='account-profile-section']/div/div/div[2]/div/span"));
+            IWebElement ProfileName = driver.FindElement(By.XPath("//div[@id='account-profile-section']/div/div/div[2]/div/span"));
+            string profilenametext = ProfileName.Text;
+            Assert.AreEqual(profilenametext, ExpectedProfilename);
+        }
+
+        [Then("click on Skill tab")]
+        public void ThenClickOnSkillTab()
+        {
+            skillPage.Skilltabclick(driver).Click();
+        }
+
+        [When("the user clicks logs in with valid credentials {string} and {string}")]
+        public void WhenTheUserClicksLogsInWithValidCredentialsAnd(string username, string password)
+        {
+            loginPage.Login(username, password);
+
+        }
+
+        [When("the user clicks on AddNew button enters {string} and {string} and clicks on Add button in the skills section")]
+        public void WhenTheUserClicksOnAddNewButtonEntersAndAndClicksOnAddButtonInTheSkillsSection(string skill, string skillLevel)
+        {
+            skillPage.SkillsPageRecordCreation(skill, skillLevel);
+        }
+
+
+        [Then("the {string} skill record should be displayed in the table")]
+        public void WhenTheRecordShouldBeDisplayedInTheTable(string skillslastcolumn)
+        {
+            Assert.AreEqual(skillslastcolumn, skillPage.SkillLastData(driver).Text);
+        }
+        [Then("the user clicks on the edit icon and updates the skill level{string} and clicks on update button")]
+        public void ThenTheUserClicksOnTheEditIconAndUpdatesTheSkillLevelAndClicksOnUpdateButton(string updatedskilllevel)
+        {
+            skillPage.SkillsPageRecordUpdate(updatedskilllevel);
+        }
+
+        [Then("the <UpdatedSkillLevel> skill level should get updated successfully")]
+        public void ThenTheUpdatedSkillLevelSkillLevelShouldGetUpdatedSuccessfully()
+        {
+            Assert.AreEqual("Intermediate", skillPage.SkillDataUpdated(driver).Text);
+
+        }
+
+
+        [Then("verify the Add skill validations and also when entering  {string} and {string} are working")]
+        public void ThenVerifyTheAddSkillValidationsAreWorking(string skill, string skilllevel)
+        {
+            skillPage.SkillsPageValidationCheckwithnodata();
+            IWebElement ErrorMessage = driver.FindElement(By.XPath("//div[@class='ns-box-inner']"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+            skillPage.SkillsPageValidationCheckwithdata(skill, skilllevel);
+            helpers.WaitForElement(By.XPath("//div[@class='ns-box-inner']"));
+            IWebElement ErrorMessage1 = driver.FindElement(By.XPath("//div[@class='ns-box-inner']"));
+            Assert.AreEqual("This skill is already exist in your skill list.", ErrorMessage1.Text);
+
+        }
+
+        [Then("verify the Edit skill validations are working")]
+        public void ThenVerifyTheEditSkillValidationsAreWorking()
+        {
+
+            skillPage.SkillEditicon(driver).Click();
+            skillPage.Skillfieldvalue(driver).Clear();
+            skillPage.SkillUpdatebutton(driver).Click();
+            helpers.WaitForElement(By.XPath("//div[@class='ns-box-inner']"));
+            IWebElement ErrorMessage = driver.FindElement(By.XPath("//div[@class='ns-box-inner']"));
+            Assert.IsTrue(ErrorMessage.Displayed);
+        }
+
+        [When("the user clicks on the cross icon against the <skill> skill record")]
+        public void WhenTheUserClicksOnTheCrossIconAgainstTheSkillSkillRecord()
+        {
+            skillPage.SkillsPageDeleteRecord();
+
+        }
+
+        [Then("the {string} skill record should be deleted")]
+
+        public void ThenTheSkillSkillRecordShouldBeDeleted(string skill)
+        {
+            helpers.WaitForElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            IWebElement DatainLastRow = driver.FindElement(By.XPath("//div[@data-tab='second']/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            Thread.Sleep(3000);
+            Assert.AreNotEqual(skill, DatainLastRow.Text);
+        }
+
+
 
     }
 }
